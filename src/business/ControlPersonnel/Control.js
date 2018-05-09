560f3a407aae1830107ff7f2899f307b2ff248cf
zhang <zhangrui_web@163.com>
2018-05-09 15:05:08 +0800

    change

--- src/business/ControlPersonnel/Control.js
+++ src/business/ControlPersonnel/Control.js
@@ -275,7 +275,7 @@ export  class Control extends Component{
             title: '联系电话',
             dataIndex: 'phone',
         },{
-            title: '隶属任务',
+            title: '责任单位',
             dataIndex: 'zrdw',
         },{
             title: '任务周期',
@@ -798,8 +798,8 @@ const SearchArea = React.createClass({
                     <Option value="暂住">暂住</Option>
                     <Option value="流动">流动</Option>
                 </Select>
-                <label htmlFor="" className="font14">隶属任务：</label>
-                <Input style={{width:'130px',marginRight:"10px"}} type="text"  id='name' placeholder='请输入隶属任务'  value={WorkPlace}  onChange={this.WorkPlaceChange}/>
+                <label htmlFor="" className="font14">责任单位：</label>
+                <Input style={{width:'130px',marginRight:"10px"}} type="text"  id='name' placeholder='请输入责任单位'  value={WorkPlace}  onChange={this.WorkPlaceChange}/>
                 <label htmlFor="" className="font14">任务周期：</label>
                 <Select value={cycle} style={{ width: 100 ,marginRight:"10px" }} onChange={this.cycleChange} notFoundContent='暂无'>
                     <Option value="">全部</Option>
