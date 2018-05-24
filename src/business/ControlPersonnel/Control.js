04a396a04f25b44d890bcc04883c8d4d9b721662
zhang <zhangrui_web@163.com>
2018-05-24 13:16:52 +0800

    change

--- src/business/ControlPersonnel/Control.js
+++ src/business/ControlPersonnel/Control.js
@@ -545,7 +545,7 @@ export  class Control extends Component{
                                     {getFieldDecorator('address', {
                                         initialValue:this.state.modalType === 'edit' ? detail.work_address : '',
                                     })(
-                                        <Input disabled/>
+                                        <Input title={detail.work_address} disabled/>
                                     )}
                                 </FormItem>
                             </Col>
