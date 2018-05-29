617d3f90c61ce7e8d4d4d93662c682f52663e001
zhangying <867181561@qq.com>
2018-05-29 16:09:10 +0800

    配置地址整理

--- src/configUrl.js
+++ src/configUrl.js
@@ -1,33 +1,27 @@
 global.configUrl = {
   //系统名称
   sysName: '反恐利剑信息化作战系统',
-  //自己 http://172.19.12.217:8071
-  //洛阳  http://10.58.45.4:8100
-  //洛阳 linux  http://10.58.38.173:8100
-  //佳木斯http://10.116.1.215:8100
-  //czp http://172.19.12.232:8081
-  //天津  http://20.3.2.31:8100
-  //重庆  http://10.158.35.98:8100
-  //测试主机  http://172.19.1.140:8100   http://172.19.4.4:7070
-  // securityCenterUrl: "http://172.19.1.145:8100",//平时测试接口
-  // securityCenterUrls: "http://172.19.12.165:8080",//跳转安全中心
-  // loginUrl: "http://172.19.12.165:8080",//登陆地址
-  // serverUrls:"http://172.19.12.213:8088/data-service",
-  // loginUrl: "http://172.19.12.249:8100",//登陆地址
-  //   securityCenterUrl: "http://172.19.12.249:8200",//平时测试接口
-  //   loginUrl: "http://172.19.12.249:8100",
-  //securityCenterUrl: "http://35.0.40.53:8100",
-  //securityCenterUrl: "http://10.148.77.204:8100",//正式打包环境
-  //洛阳http://10.58.45.4:7000 http://172.19.12.147:7077    data-service 反向代理
-  //佳木斯/data-service 反向代理
-  // /data-service-wwhc  测试临时使用
-  //wyn http://172.19.12.147:8888
-  /*  serverUrl: "http://172.19.12.147:8888",*/
-  //serverUrl: "/data-service",
-  //serverUrl: "http://172.19.12.137:8888",//shd 服务地址
-  //http://172.19.1.145:7000 测试部接口地址
-  securityCenterUrl: "http://172.19.12.165:8080",//张旭接口
-  serverUrl: "http://172.19.12.102:8800",
+
+  Interface: {//服务器
+    luoyang: 'http://10.58.45.4:8100',
+    jiamusi: 'http://10.116.1.215:8100',
+    tianjin: 'http://20.3.2.31:8100',
+    chongqing: 'http://10.158.35.98:8100',
+    ceshizuji1: 'http://172.19.1.140:8100',
+    ceshizuji2: 'http://172.19.4.4:7070',
+    test: 'http://172.19.1.145:7000',
+    fxdl: '/data-service',
+    lsfxdl: '/data-service-wwhc',
+    MXD: 'http://172.19.12.102:8800',//慕兴达
+    ZZH: 'http://172.19.12.213:8088/data-service',//张泽恒
+  },
+  securityCenter: {//安全中心
+    test: 'http://172.19.1.145:8100',
+    ZX: 'http://172.19.12.165:8080',//张旭接口
+    pingshiTest: 'http://172.19.12.249:8200',
+  },
+  securityCenterUrl: securityCenter.ZX,
+  serverUrl: Interface.MXD,
   //客户区域
   clientArea: '黑龙江',
   //PKI登录是否开启
