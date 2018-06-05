400a26befbbff6104b1aece6115b3d028eedab57
zhangying <867181561@qq.com>
2018-06-05 12:04:42 +0800

    首页样式 退出登录

--- src/actions/login.js
+++ src/actions/login.js
@@ -128,6 +128,7 @@ export function logoutUser() {
         sessionStorage.removeItem('user');
         dispatch(receiveLogout());
         browserHistory.push('/');
+        location.replace('/') 
     }
 }
 
